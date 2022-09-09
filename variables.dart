340d75783c6612977e60c18c5f6b430aa97947e4
Felipe Printes <felipeprintes@MacBook-Air-de-Felipe.local>
2022-09-09 07:48:14 -0300

    add variables.dart

---
+++ variables.dart
@@ -0,0 +1,12 @@
+void main() {
+  int idade = 26;
+  double salario = 89.91;
+  bool empregado = false;
+  String nome = 'Fulano Silveira';
+
+  if (empregado) {
+    print("Meu nome é: $nome, tenho um salário de $salario e tenho $idade");
+  } else {
+    print("Não sou empregado ainda");
+  }
+}
